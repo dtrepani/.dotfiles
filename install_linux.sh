88192b47c80c5a0c7d33195000ee8d9c262bdc48
Fina <finapani@gmail.com>
2015-09-30 13:35:09 -0400

    Added script for linking files on Linux.

---
+++ install_linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Link each dotfile in the directory to its home counterpart.
+# Reminders:
+#		- Install Powerline Fonts (Currently used: DejaVu 10pt)
+#		- Add Pathogen:
+#			- curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
+#			- cd ~/.vim/bundle && git clone git://github.com/tpope/vim-sensible.git
+#		- Add Vundle: git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+#			- Run Vundle :PluginInstal
+
+shopt -s extglob
+
+for file in ~/.dotfiles/!(install*|dotbot*) ; do
+	filename=$(basename $file)
+	echo "-- Linking: $filename"
+	[ -f ~/.$filename ] && rm ~/.$filename && echo "Removed ~/.$filename"
+	ln -s ~/.dotfiles/$filename ~/.$filename
+done
